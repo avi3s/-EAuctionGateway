d15de4d9f15c313145e8cf60add8164a7668b2b2
avi3s <avirup.pal@gmail.com>
2022-08-23 11:28:38 +0530

    first commit

---
+++ src/test/java/com/eauction/gateway/EauctionGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eauction.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EauctionGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
